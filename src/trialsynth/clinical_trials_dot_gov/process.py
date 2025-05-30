008230e0a404dd73b0fb49caa95c33f76b1d2fbc
kkaris <karis.klas@gmail.com>
2025-05-30 12:45:20 -0700

    Add docstring to CTProcesor's __init__

--- src/trialsynth/clinical_trials_dot_gov/process.py
+++ src/trialsynth/clinical_trials_dot_gov/process.py
@@ -16,6 +16,21 @@ class CTProcessor(Processor):
         condition_grounder: Grounder = CTConditionGrounder(),
         intervention_grounder: Grounder = CTInterventionGrounder(),
     ):
+        """Initialize the ClinicalTrials.gov processor.
+
+        Parameters
+        ----------
+        reload_api_data :
+            If True, the API data will be reloaded from the ClinicalTrials.gov REST API.
+        store_samples :
+            If True, the processor will store sample data for validation.
+        validate :
+            If True, the processor will validate the data.
+        condition_grounder :
+            A grounder to use for grounding conditions. Defaults to CTConditionGrounder().
+        intervention_grounder :
+            A grounder to use for grounding interventions. Defaults to CTInterventionGrounder().
+        """
         super().__init__(
             config=CTConfig(),
             fetcher=CTFetcher(CTConfig()),
